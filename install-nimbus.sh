1b767ccba3c6e42ea9aacd97e22ad184fb12fd51
abhishek <abhishek.tripathi@inmobi.com>
2018-04-04 16:59:19 +0530

    UI fix

--- install-nimbus.sh
+++ install-nimbus.sh
@@ -4,4 +4,4 @@ sudo docker run -d --restart always -p 2181:2181 zookeeper
 
 sudo docker run -d --restart always --name=$1  -e NIMBUS_SEEDS=$1 -e ZK1=$2 -e ZK_PORT=2181 -p 6627:6627 abhishektripathi138/storm-docker storm nimbus
 
-sudo docker run -d --restart always --link abhishektripathi138/storm-docker -e NIMBUS_SEEDS=$1 -e ZK1=$2 -e ZK_PORT=2181 -p 8099:8099 abhishektripathi138/storm-docker storm ui
+sudo docker run -d --restart always --link $1 -e NIMBUS_SEEDS=$1 -e ZK1=$2 -e ZK_PORT=2181 -p 8099:8099 abhishektripathi138/storm-docker storm ui
