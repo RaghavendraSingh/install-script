b5cba71b16b7ff8433e2a4dade475ca3d55dc1e1
abhishek <abhishek.tripathi@inmobi.com>
2018-04-04 18:25:39 +0530

    UI fix

--- install-supervisor.sh
+++ install-supervisor.sh
@@ -1,1 +1,3 @@
+bash docker-install.sh
+
 sudo docker run -d --restart always -e NIMBUS_SEEDS=$2 -e ZK1=$2 -e ZK_PORT=2181 -p 6700:6700 -p 6701:6701 -p 6702:6702 -p 6703:6703 abhishektripathi138/storm-docker storm supervisor
