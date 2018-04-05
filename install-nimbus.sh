5fcd68c8dfb7cdf56822834df172739ee97fe313
abhishek <abhishek.tripathi@inmobi.com>
2018-04-05 20:48:54 +0530

    arrayfixes

--- install-nimbus.sh
+++ install-nimbus.sh
@@ -2,8 +2,7 @@ bash docker-install.sh
 
 #sudo docker run -d --restart always -p 2181:2181 zookeeper
 
-IFS=',' read -ra ADDR <<< "$2"
-
+ADRR=(${2//,/ })
 
 sudo docker run -d --restart always --name=$1  -e NIMBUS_SEEDS=$1 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6627:6627 abhishektripathi138/storm-docker storm nimbus
 
