7f2bd7a1ccbf9b53d1a889204d6676a188b23798
abhishek <abhishek.tripathi@inmobi.com>
2018-04-05 17:27:41 +0530

    fixes

--- install-nimbus.sh
+++ install-nimbus.sh
@@ -5,6 +5,6 @@ bash docker-install.sh
 IFS=',' read -ra ADDR <<< "$2"
 
 
-sudo docker run -d --restart always --name=$1  -e NIMBUS_SEEDS=$1 -e ZK1=ADRR[0] -e ZK2=ADRR[1] -e ZK3=ADRR[2] -e ZK_PORT=2181 -p 6627:6627 abhishektripathi138/storm-docker storm nimbus
+sudo docker run -d --restart always --name=$1  -e NIMBUS_SEEDS=$1 -e ZK1=$ADRR[0] -e ZK2=$ADRR[1] -e ZK3=$ADRR[2] -e ZK_PORT=2181 -p 6627:6627 abhishektripathi138/storm-docker storm nimbus
 
-sudo docker run -d --restart always --link $1 -e NIMBUS_SEEDS=$1 -e ZK1=ADRR[0] -e ZK2=ADRR[1] -e ZK3=ADRR[2] -e ZK_PORT=2181 -p 8099:8099 abhishektripathi138/storm-docker storm ui
+sudo docker run -d --restart always --link $1 -e NIMBUS_SEEDS=$1 -e ZK1=$ADRR[0] -e ZK2=$ADRR[1] -e ZK3=$ADRR[2] -e ZK_PORT=2181 -p 8099:8099 abhishektripathi138/storm-docker storm ui
