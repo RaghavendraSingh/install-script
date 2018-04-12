5f4fb46b996781797a3a3bbd166134b287a0fab1
abhishek <abhishek.tripathi@inmobi.com>
2018-04-12 14:24:52 +0530

    Nimbus HA

--- install-nimbus.sh
+++ install-nimbus.sh
@@ -2,8 +2,8 @@ bash docker-install.sh
 
 #sudo docker run -d --restart always -p 2181:2181 zookeeper
 
-ADRR=(${2//,/ })
+ADRR=(${3//,/ })
 
-sudo docker run -d --restart always --name=$1  -e NIMBUS_SEEDS=$1 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6627:6627 abhishektripathi138/storm-docker storm nimbus
+sudo docker run -d --restart always --name=$1  -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6627:6627 abhishektripathi138/storm-docker storm nimbus
 
-sudo docker run -d --restart always --link $1 -e NIMBUS_SEEDS=$1 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 8099:8099 abhishektripathi138/storm-docker storm ui
+sudo docker run -d --restart always --link $1 -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 8099:8099 abhishektripathi138/storm-docker storm ui
