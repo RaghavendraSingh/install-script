f83d61c1361743bcea8beeb92958894a4cc3d993
abhishek <abhishek.tripathi@inmobi.com>
2018-04-19 16:36:33 +0530

    docker login changes

--- install-nimbus.sh
+++ install-nimbus.sh
@@ -4,6 +4,8 @@ bash docker-install.sh
 
 ADRR=(${3//,/ })
 
-sudo docker run -d --restart always --name=$1  -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6627:6627 abhishektripathi138/storm-docker storm nimbus
+sudo docker login $4 --username="$5" --password="$6"
 
-sudo docker run -d --restart always --link $1 -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 8099:8099 abhishektripathi138/storm-docker storm ui
+sudo docker run -d --restart always --name=$1  -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6627:6627 $4/storm-docker storm nimbus
+
+sudo docker run -d --restart always --link $1 -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 8099:8099 $4/storm-docker storm ui
--- install-supervisor.sh
+++ install-supervisor.sh
@@ -2,4 +2,6 @@ bash docker-install.sh
 
 ADRR=(${3//,/ })
 
-sudo docker run -d --restart always -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6700:6700 -p 6701:6701 -p 6702:6702 -p 6703:6703 abhishektripathi138/storm-docker storm supervisor
+sudo docker login $4 --username="$5" --password="$6"
+
+sudo docker run -d --restart always -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6700:6700 -p 6701:6701 -p 6702:6702 -p 6703:6703 $4/storm-docker storm supervisor
