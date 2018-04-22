6dc3329bb8ac03764993704b2705cf838eb97757
abhishek <abhishek.tripathi@inmobi.com>
2018-04-23 00:29:25 +0530

    Fixes

--- install-supervisor.sh
+++ install-supervisor.sh
@@ -6,11 +6,11 @@ ADRR=(${3//,/ })
 sudo docker login $4 --username="$5" --password="$6"
 
 sudo mkdir -p /var/log/storm
-dockerId="$(sudo docker run -d --restart always -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6700:6700 -p 6701:6701 -p 6702:6702 -p 6703:6703 -v ./logs:/var/log/storm $4/storm-docker storm supervisor)"
-
-mkdir -p 
+dockerId="$(sudo docker run -d --restart always -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6700:6700 -p 6701:6701 -p 6702:6702 -p 6703:6703 -v /var/log/storm:/logs $4/storm-docker storm supervisor)"
+echo $dockerId
+sudo mkdir -p /fileshare/stormlogs/$dockerId/
 
 sudo crontab -l > mycron
-echo "30 00 * * * rsync -av /var/log/storm /fileshare/stormlogs/$dockerId/" >> mycron
-crontab mycron
-rm mycron
+sudo echo "30 00 * * * rsync -av /var/log/storm /fileshare/stormlogs/$dockerId/" >> mycron
+sudo crontab mycron
+sudo rm mycron
--- mount-blob-container.sh
+++ mount-blob-container.sh
@@ -2,12 +2,13 @@ wget https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 sudo apt-get install blobfuse
+sudo mkdir -p /mnt/ramdisk
 sudo mount -t tmpfs -o size=16g tmpfs /mnt/ramdisk
-sudo mkdir /mnt/ramdisk/blobfusetmp
+sudo mkdir -p /mnt/ramdisk/blobfusetmp
 touch fuse_connection.cfg
 echo "accountName $1" >> fuse_connection.cfg
 echo "accountKey $2" >> fuse_connection.cfg
 echo "containerName $3" >> fuse_connection.cfg
 chmod 700 fuse_connection.cfg
 sudo mkdir /fileshare
-blobfuse /fileshare --tmp-path=/mnt/ramdisk/blobfusetmp  --config-file=/path/to/fuse_connection.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120
+sudo blobfuse /fileshare --tmp-path=/mnt/ramdisk/blobfusetmp  --config-file=fuse_connection.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120
