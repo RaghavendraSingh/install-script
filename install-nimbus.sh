616feaa070e2bfc6189acecf9281430c3a065568
abhishek <abhishek.tripathi@inmobi.com>
2018-04-24 14:59:10 +0530

    Docker hostname fixes

--- install-nimbus.sh
+++ install-nimbus.sh
@@ -6,6 +6,6 @@ ADRR=(${3//,/ })
 
 sudo docker login $4 --username="$5" --password="$6"
 
-sudo docker run -d --restart always --name=$1  -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6627:6627 $4/storm-docker storm nimbus
+sudo docker run -d --restart always --name=$1  -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6627:6627 -h $1 $4/storm-docker storm nimbus
 
 sudo docker run -d --restart always --link $1 -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 8099:8099 $4/storm-docker storm ui
--- install-supervisor.sh
+++ install-supervisor.sh
@@ -6,7 +6,8 @@ ADRR=(${3//,/ })
 sudo docker login $4 --username="$5" --password="$6"
 
 sudo mkdir -p /var/log/storm
-dockerId="$(sudo docker run -d --restart always -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6700:6700 -p 6701:6701 -p 6702:6702 -p 6703:6703 -v /var/log/storm:/logs $4/storm-docker storm supervisor)"
+privateIp="$(hostname -i)"
+dockerId="$(sudo docker run -d --restart always -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6700:6700 -p 6701:6701 -p 6702:6702 -p 6703:6703 -v /var/log/storm:/logs -h $privateIp $4/storm-docker storm supervisor)"
 echo $dockerId
 sudo mkdir -p /fileshare/stormlogs/$dockerId/
 
