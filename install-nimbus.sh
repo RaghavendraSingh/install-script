edf8e94854632ca9538d3cc7e5fbe8a980460fac
abhishek <abhishek.tripathi@inmobi.com>
2018-05-03 14:41:58 +0530

    Collectd changes

--- install-nimbus.sh
+++ install-nimbus.sh
@@ -3,9 +3,11 @@ bash docker-install.sh
 #sudo docker run -d --restart always -p 2181:2181 zookeeper
 
 ADRR=(${3//,/ })
-
+logDir=/var/log/storm/
+dataDir=/mnt/storm	
 sudo docker login $4 --username="$5" --password="$6"
-
-sudo docker run -d --restart always --name=$1  -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6627:6627 -h $1 $4/storm-docker storm nimbus
-
-sudo docker run -d --restart always --link $1 -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 8099:8099 $4/storm-docker storm ui
+if [ $# ge 7 ]; then
+sudo docker run -d --restart always --name=$1  -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6627:6627 -e GRAPHITE_HOST=${7} -e GRAPHITE_PORT=${8} -h $1 -v $logDir:/logs -v $dataDir:/data $4/storm-docker storm nimbus
+else
+sudo docker run -d --restart always --link $1 -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 8099:8099 -v $logDir:/logs -v  $dataDir:/data $4/storm-docker storm ui
+fi
--- install-supervisor.sh
+++ install-supervisor.sh
@@ -1,6 +1,7 @@
 bash docker-install.sh
 privateIp="$(hostname -i)"
 logDir=/var/log/storm/
+dataDir=/mnt/storm	
 mountDir=/fileshare
 bash mount-blob-container.sh $7 $8 $9 $mountDir
 mountDir=$mountDir/$1/$privateIp
@@ -8,9 +9,11 @@ sudo mkdir -p $mountDir
 ADRR=(${3//,/ })
 sudo docker login $4 --username="$5" --password="$6"
 
-#sudo mkdir -p /var/log/storm
-dockerId="$(sudo docker run -d --restart always -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6700:6700 -p 6701:6701 -p 6702:6702 -p 6703:6703 -v $logDir:/logs -h $privateIp $4/storm-docker storm supervisor)"
-
+if [ $# ge 10 ]; then
+dockerId="$(sudo docker run -d --restart always -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -e GRAPHITE_HOST=${10} -e GRAPHITE_PORT=${11} -p 6700:6700 -p 6701:6701 -p 6702:6702 -p 6703:6703 -v $logDir:/logs -v  $dataDir:/data -h $privateIp $4/storm-docker storm supervisor)"
+else
+dockerId="$(sudo docker run -d --restart always -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6700:6700 -p 6701:6701 -p 6702:6702 -p 6703:6703 -v $logDir:/logs -v  $dataDir:/data -h $privateIp $4/storm-docker storm supervisor)"
+fi
 sudo crontab -l > mycron
 sudo echo "05 * * * * rsync -av /var/log/storm $mountDir" >> mycron
 sudo crontab mycron
