1b8c2124f7cc06552be49fdea82e402a505e62c9
abhishek <abhishek.tripathi@inmobi.com>
2018-05-06 12:03:17 +0530

    Fixes

--- install-nimbus.sh
+++ install-nimbus.sh
@@ -10,7 +10,8 @@ sudo docker login $4 --username="$5" --password="$6"
 if [ $# -ge 7 ]; then
 sudo docker pull $4/storm-docker:latest && sudo docker run -d --restart always --name=$1  -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6627:6627 -e GRAPHITE_HOST=${7} -e GRAPHITE_PORT=${8} -h $1 -v $logDir:/logs -v $dataDir:/data $4/storm-docker storm nimbus
 else
-sudo docker pull $4/storm-docker:latest && sudo docker run -d --restart always --link $1 -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 8099:8099 -v $logDir:/logs -v  $dataDir:/data $4/storm-docker storm ui
+sudo docker pull $4/storm-docker:latest && sudo docker run -d --restart always --name=$1  -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6627:6627 -h $1 -v $logDir:/logs -v $dataDir:/data $4/storm-docker storm nimbus
 fi
+sudo docker pull $4/storm-docker:latest && sudo docker run -d --restart always --link $1 -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 8099:8099 -v $logDir:/logs -v  $dataDir:/data $4/storm-docker storm ui
 
 
---
+++ install-storm-client.sh
@@ -0,0 +1,1 @@
+wget https://archive.apache.org/dist/storm/apache-storm-1.1.0/apache-storm-1.1.0.tar.gz
