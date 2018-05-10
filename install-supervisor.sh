b523db24a3f2455d7be477acdee651bed88e6e1c
abhishek <abhishek.tripathi@inmobi.com>
2018-05-10 18:25:51 +0530

    Removed private ip

--- install-supervisor.sh
+++ install-supervisor.sh
@@ -10,9 +10,9 @@ ADRR=(${3//,/ })
 sudo docker login $4 --username="$5" --password="$6"
 sudo docker pull $4/storm-docker:latest
 if [ $# -ge 10 ]; then
-dockerId="$(sudo docker run -d --restart always -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -e GRAPHITE_HOST=${10} -e GRAPHITE_PORT=${11} -p 6700:6700 -p 6701:6701 -p 6702:6702 -p 6703:6703 -v $logDir:/logs -v  $dataDir:/data -h $privateIp $4/storm-docker storm supervisor)"
+dockerId="$(sudo docker run -d --restart always -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -e GRAPHITE_HOST=${10} -e GRAPHITE_PORT=${11} -p 6700:6700 -p 6701:6701 -p 6702:6702 -p 6703:6703 -v $logDir:/logs -v  $dataDir:/data $4/storm-docker storm supervisor)"
 else
-dockerId="$(sudo docker run -d --restart always -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6700:6700 -p 6701:6701 -p 6702:6702 -p 6703:6703 -v $logDir:/logs -v  $dataDir:/data -h $privateIp $4/storm-docker storm supervisor)"
+dockerId="$(sudo docker run -d --restart always -e NIMBUS1=$1 -e NIMBUS2=$2 -e ZK1=${ADRR[0]} -e ZK2=${ADRR[1]} -e ZK3=${ADRR[2]} -e ZK_PORT=2181 -p 6700:6700 -p 6701:6701 -p 6702:6702 -p 6703:6703 -v $logDir:/logs -v  $dataDir:/data $4/storm-docker storm supervisor)"
 fi
 sudo crontab -l > mycron
 sudo echo "05 * * * * rsync -av /var/log/storm $mountDir" >> mycron
