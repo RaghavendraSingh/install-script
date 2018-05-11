886cf28ffb9d8f9991a70f62c4a7c956be4f44d4
abhishek <abhishek.tripathi@inmobi.com>
2018-05-11 15:07:33 +0530

    Storm client changes

--- install-storm-client.sh
+++ install-storm-client.sh
@@ -1,1 +1,6 @@
 wget https://archive.apache.org/dist/storm/apache-storm-1.1.0/apache-storm-1.1.0.tar.gz
+tar -zxvf apache-storm-1.1.0.tar.gz
+cd apache-storm-1.1.0/bin
+binPath="$(pwd)"
+echo "export PATH=$PATH:$binPath" >> ~/.bashrc
+export PATH=$PATH:$binPath
