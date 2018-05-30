6deed114284f1c1af4e1dec9863805cf5bad9633
abhishek <abhishek.tripathi@inmobi.com>
2018-05-30 18:45:04 +0530

    zk install

--- install-zk.sh
+++ install-zk.sh
@@ -8,5 +8,5 @@ sudo docker login $3 --username="$4" --password="$5"
 GRAPHITE_HOST=${11}
 
 # enabling metrics if graphite host is set
-sudo docker pull $4/storm-docker:latest && sudo docker run -d --restart always -e ZK_IPS=$1 -e ZK_ID=$2 -v $mountDir:/logs -v $dataDir:/data $3/storm-docker /zookeeper-3.4.8/bin/zkServer.sh start
+sudo docker pull $4/storm-docker:latest && sudo docker run -d --restart always -e ZK_IPS=$1 -e ZK_ID=$2 -v $mountDir:/logs -v $dataDir:/data $3/zk-docker /zookeeper-3.4.8/bin/zkServer.sh start
 
