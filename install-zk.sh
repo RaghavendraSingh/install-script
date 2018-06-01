0ea020fc73323079078806f6bc6ab9adab1beb96
abhishek <abhishek.tripathi@inmobi.com>
2018-06-01 15:10:26 +0530

    Docker changes ubuntu 16

--- install-zk.sh
+++ install-zk.sh
@@ -6,5 +6,5 @@ sudo docker login $3 --username="$4" --password="$5"
 #GRAPHITE_HOST=${11}
 
 # enabling metrics if graphite host is set
-sudo docker pull $3/zk-docker:latest && sudo docker run -d --restart always -e ZK_IPS=$1 -e ZK_ID=$2 -v $logDir:/logs -v $dataDir:/data -p 2181:2181 $3/zk-docker /zookeeper-3.4.8/bin/zkServer.sh start tail -F /dev/null
+sudo docker pull $3/zk-docker:latest && sudo docker run -d --restart always -e ZK_IPS=$1 -e ZK_ID=$2 -v $logDir:/logs -v $dataDir:/data -p 2181:2181 $3/zk-docker 
 
