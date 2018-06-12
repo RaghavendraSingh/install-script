8896cdcaa949e1527dfad6bf0cd2872c53a61738
abhishek <abhishek.tripathi@inmobi.com>
2018-06-12 13:44:59 +0530

    Java installation fix

--- install-storm-client.sh
+++ install-storm-client.sh
@@ -1,14 +1,10 @@
 #install java
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-x64.tar.gz"
-tar -xvf jdk-8*
-mkdir /usr/lib/jvm
-mv ./jdk1.8* /usr/lib/jvm/jdk1.8.0
-update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.0/bin/java" 1
-update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.8.0/bin/javac" 1
-update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.8.0/bin/javaws" 1
+add-apt-repository ppa:openjdk-r/ppa
+apt-get update
+apt-get install openjdk-8-jdk
+update-alternatives --config java
 chmod a+x /usr/bin/java
 chmod a+x /usr/bin/javac
-chmod a+x /usr/bin/javaws
 
 user=$1
 cd /home/$user
